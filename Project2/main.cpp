6259c000c0a6ada7110f37e3bea844e14e92f76d
rickvdp93 <rick.vanderpaauw@student.hu.nl>
2014-02-10 13:57:49 +0100

    opdracht1.1+1.2

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,12 +1,56 @@
 #include "main.h"
 #include <iostream>
 #include <string>
-#include "C:/libs/corona-1.0.2-vc6/include/corona.h"
+#include <fstream>
+#include "corona.h"
 using namespace std;
 
-main::main()
+void main()
 {	
-	string filename = "C:/pic.png";
-	corona::Image* input = corona::OpenImage(filename.c_str, corona::PF_B8G8R8A8);
-	corona::SaveImage("C:/grey_" + filename.c_str, corona::PF_B8G8R8A8, input);
+	string filename = "C:/gtr.png";
+	string filename2 = "C:/gtr2.png";
+	
+	corona::Image* image = corona::OpenImage(filename.c_str(), corona::PF_B8G8R8A8);
+	if (!image){
+		cout << "Missing file";
+		return;
+	}
+
+	ofstream outFile;
+	outFile.open("C:/csv.csv");
+	if (outFile.fail()) {
+		cout << "csv write open failed";
+		return;
+	}
+
+
+	int width = image->getWidth();
+	int height = image->getHeight();
+	void* pixels = image->getPixels();
+
+	typedef unsigned char byte;
+	unsigned int greyOccurrence[256] = { 0 };
+	byte* p = (byte*)pixels;
+	byte* d = (byte*)pixels;
+
+	for (int i = 0; i < width*height; i++){
+		byte red = *p++;
+		byte green = *p++;
+		byte blue = *p++;
+		byte alpha = *p++;
+		byte grey = ((red + green + blue) / 3);
+		*d++ = grey;
+		*d++ = grey;
+		*d++ = grey;
+		*d++ = alpha;
+		greyOccurrence[grey] += 1;
+	}
+	for (int i = 0; i < 256; i++){
+		outFile << i;
+		outFile << ";";
+		outFile << (float)greyOccurrence[i]/(width*height);
+		outFile << endl;
+	}
+	bool output = corona::SaveImage(filename2.c_str(), corona::FF_PNG, image);
+	outFile.close();
 }
